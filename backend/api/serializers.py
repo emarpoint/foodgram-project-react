fc9b3587f217c496c5faf387889a7392c4a1adb7
Егор Марецкий <emarpoint@gmail.com>
2022-06-13 12:37:02 +0300

    Commit 13.06.22

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -6,7 +6,6 @@ from django.core.validators import RegexValidator
 from djoser.serializers import UserCreateSerializer
 from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
-
 from recipes.models import (Favorite, Ingredient, IngredientRecipe, Recipe,
                             ShoppingCart, Subscribe, Tag, TagRecipe)
 from users.models import CustomUser
