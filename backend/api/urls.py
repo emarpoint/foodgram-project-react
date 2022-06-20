b9ce1e47a36b0738f04d924d4e64925ef6f6fb30
Егор Марецкий <emarpoint@gmail.com>
2022-06-20 18:28:02 +0300

    Commit 20.06.2022

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,7 +2,8 @@
 Создание маршрутизаторов API проекта Foodgram.
 Creating Foodgram API routers.
 """
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
@@ -37,3 +38,12 @@ urlpatterns = [
     path('', include(router.urls)),
     path('auth/', include('djoser.urls.authtoken')),
 ]
+
+
+
+if settings.DEBUG:
+    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
