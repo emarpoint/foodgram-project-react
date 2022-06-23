fa8ffb7ebae78d8b045caf1d497bd9a48ff14574
Егор Марецкий <emarpoint@gmail.com>
2022-06-23 18:29:04 +0300

    Final

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -119,6 +119,14 @@ DATABASES = {
     }
 }
 
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 AUTH_USER_MODEL = 'users.CustomUser'
 
 
